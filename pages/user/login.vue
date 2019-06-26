4667806257ebc63a2ca0acf88d0a39846672a0cc
Asad <17336189682@189.cn>
2019-06-26 19:31:20 +0800

    完成头部和脚部组件

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -1,16 +1,12 @@
 <template>
-  <div class="container">
-      
-  </div>
+    <div>
+        登录注册页面
+    </div>
 </template>
 
 <script>
-
-
 export default {
-  components: {
-  
-  }
+
 }
 </script>
 
