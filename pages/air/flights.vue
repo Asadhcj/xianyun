c798a0af5eaa8d5965a166f176aa07579c2342d3
Asad <17336189682@189.cn>
2019-07-03 08:43:54 +0800

    完成机票列表页渲染和部分过滤

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,15 +1,108 @@
 <template>
-    <div>
-        机票列表
+    <div class="cantainter">
+       <div class="main">
+           <el-row type="flex" justify="space-between">
+               <div class="flights-left">
+                    <FLIGHTSFILTERS :data="datalist" @select="getflightslist"/>
+                    <FLIGHTSHEADER/>
+                    <div>
+                        <FLIGHTSCONTENT
+                        v-for="(item,index) in data.flights" 
+                        :key="index"
+                        :ticketInfo="item"/> 
+                        <el-pagination
+                        @size-change="handleSizeChange"
+                        @current-change="handleCurrentChange"
+                        :current-page="pageIndex"
+                        :page-sizes="[5, 10, 15, 20]"
+                        :page-size="pageSize"
+                        layout="total, sizes, prev, pager, next, jumper"
+                        :total="data.flights.total">
+                        </el-pagination>
+                    </div>
+                  
+               </div>
+               <div class="flights-right"></div>
+           </el-row>
+       </div>
     </div>
 </template>
 
 <script>
+    import FLIGHTSFILTERS from "@/components/air/flightsFilters";
+    import FLIGHTSHEADER from "@/components/air/flightsheader";
+    import  FLIGHTSCONTENT from "@/components/air/flightscontent"
 export default {
-
+    data(){
+        return {
+            data:{
+                info:{},
+                options:{},
+                flights:[]
+            },
+            datalist:{
+                info:{},
+                options:{},
+                flights:[] 
+            },
+            pageIndex:1,
+            pageSize:5,
+            newdata:{
+                info:{},
+                options:{},
+                flights:[]
+            }
+        }
+    },
+    components:{
+         FLIGHTSFILTERS,
+         FLIGHTSHEADER,
+         FLIGHTSCONTENT
+    },
+    methods:{
+        handleSizeChange(val) {
+        this.pageSize=val;
+         this.getflightslist()
+      },
+      handleCurrentChange(val) {
+        this.pageIndex=val;
+         this.getflightslist()
+      },
+      getflightslist(arr){
+          if(arr){
+              this.pageIndex=1,
+              this.newdata.flights=arr,
+              this.pageSize=arr.length
+          }
+        this.data.flights=this.newdata.flights.slice(
+            (this.pageIndex-1)*this.pageSize,
+            this.pageSize*this.pageIndex)
+      },
+     
+    },
+    mounted(){
+        this.$axios({
+            url:"/airs",
+            method:"GET",
+            params:this.$route.query
+        }).then(res=>{
+            // console.log(res.data)
+            this.datalist={...res.data}
+            this.newdata={...res.data}
+            this.data={...res.data};
+            this.getflightslist()
+        })
+    }
+   
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+    .cantainter{
+        width:100%;
+        .main{
+            width: 1000px;
+            margin:0 auto;
+        }
+    }
 </style>
---
+++ pages/air/order.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <el-row type="flex" justify="space-between">
+            <!-- 订单表单 -->
+            <div class="main">
+                 <ORDERFORM/>
+            </div>
+
+            <!-- 侧边栏 -->
+            <div class="aside">
+                   <!-- <ORDERASIDE/>        -->
+            </div>
+        </el-row>
+    </div>
+</template>
+
+<script>
+ import ORDERFORM from "@/components/air/orderForm.vue";
+//  import ORDERASIDE from "@/components/air/orderAside.vue"
+export default {
+    components:{
+         ORDERFORM,
+        //  ORDERASIDE,
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .container{
+        width:1000px;
+        margin:20px auto;
+    }
+    
+    /*aside*/
+    .aside{
+        width: 350px;
+        height: fit-content;
+        border:1px #ddd solid;
+    }
+</style>
